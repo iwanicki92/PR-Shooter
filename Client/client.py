c5849e76a9347e95e3026cf0a6244b69c8e74045
Lukasz Plust <lukasz.plust007@wp.pl>
2022-05-19 21:36:40 +0200

    client-poprawiony

--- Client/client.py
+++ Client/client.py
@@ -1,30 +1,37 @@
-class Game():
+from keyboard import Game
+
+
+class Game_settings():
 
     def __init__(self, size, type_of_message, n, m):
         self.size = size
         self.type_of_message = type_of_message
         self.n = n
         self.m = m
-
+        self.projectiles: list[Projectile] = []
+        self.players: list[Player] = []
+        self.list_of_walls: list[list[tuple[float, float]]] = []
+        self.list_of_obstacles: list[tuple[float, float, float]] = []
 
 class Player:
 
-    def __init__(self, ident, health, x_pos, y_pos, speed, angle_of_movement, angle_of_watching):
+    def __init__(self, ident, health, x_pos, y_pos, velocity_x, velocity_y, angle_of_watching):
         self.ident = ident
         self.health = health
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.speed = speed
-        self.angle_of_movement = angle_of_movement
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
         self.angle_of_watching = angle_of_watching
 
 
 class Projectile:
-    def __init__(self, ident, x_pos, y_pos, angle_of_movement):
+    def __init__(self, ident, x_pos, y_pos, velocity_x, velocity_y):
         self.id = ident
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.angle_of_movement = angle_of_movement
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
 
 
 
